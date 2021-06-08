ddef5dc46689765897db835271b41aa03c536e30
Cansın Aldanmaz <canald@gmail.com>
2021-06-08 11:51:02 +0300

    Create user_seed.js

---
+++ app/user_seed.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        "username": "admin",
+        "email": "admin@cnsn",
+        "firstName": "admin",
+        "lastName": "cnsn",
+        "node_id": "http://kong:8001",
+        "admin": true,
+        "active" : true,
+        "password": "Admin1122"
+    },
+    {
+        "username": "demo",
+        "email": "demo@cnsn",
+        "firstName": "demo",
+        "lastName": "cnsn",
+        "node_id": "http://kong:8001",
+        "admin": false,
+        "active" : true,
+        "password": "Demo1122"
+    }
+]
